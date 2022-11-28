4dd136435a2ce0de4ec533ca130718a0eb7a9d02
Fron Tade <qoplonbekfronted@gmail.com>
2022-11-28 21:08:36 +0500

    Qale

--- home.js
+++ home.js
@@ -73,18 +73,29 @@
 
                                             ///////////////// 4 - usuli  ///////////////////////////////////
 
-    function getLastItem(arr, num){
-        if(num == null){
-            return arr[arr.length-1]
-        }
-        else if(num > 0 && num < arr.length){
-            return arr.slice(arr.length-num, arr.length)
-        }
-        else if(num > arr.length){
-             return arr
-        }
-    }
+    // function getLastItem(arr, num){
+    //     if(num == null){
+    //         return arr[arr.length-1]
+    //     }
+    //     else if(num > 0 && num < arr.length){
+    //         return arr.slice(arr.length-num, arr.length)
+    //     }
+    //     else if(num > arr.length){
+    //          return arr
+    //     }
+    // }
 
-    console.log(getLastItem([1,2,3]))
-    console.log(getLastItem(["cat", "dog", "duck"]))
-    console.log(getLastItem(["ture", "false", "ture"]))
+    // console.log(getLastItem([1,2,3]))
+    // console.log(getLastItem(["cat", "dog", "duck"]))
+    // console.log(getLastItem(["ture", "false", "ture"]))
+
+                                                /////////////////// 5 - usul ////////////////////////////////////////
+
+// function getLastItem(arr, num){
+//     return (num==null)?arr[arr.length -1]:arr.slice(-num);
+//                                                     }
+                                                    
+//     console.log(getLastItem([1,2,3]))
+//     console.log(getLastItem(["cat", "dog", "duck"]))
+//     console.log(getLastItem(["ture", "false", "ture"]))
+
